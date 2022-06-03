4fc07210f85f9d0e496f389dd8ae0a0e914683c3
Simen Dager Sneve <smndagersneve@gmail.com>
2022-06-03 17:09:45 +0200

    Generate a strong random password of fixed length

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+extern void arc4random_buf(void *buf, size_t nbytes);
+
 int main(int argc, char **argv)
 {
+  long unsigned int i;
+  char c;
+  char passwd[8];
+  arc4random_buf(passwd, sizeof(passwd));
+  for (i = 0; i < sizeof(passwd); i++) {
+    c = (passwd[i] + 128) % 93 + '!';
+    printf("%c", c);
+  }
+  printf("\n");
 }
